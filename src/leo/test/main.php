5fe3071bd5f940dc5a58075013979e4e4153a9c2
LeoWasCoding <98230552+LeoWasCoding@users.noreply.github.com>
2023-07-30 23:12:13 +0500

    Update main.php

--- src/leo/test/main.php
+++ src/leo/test/main.php
@@ -1,29 +1,31 @@
 <?php
 
-namespace leo\test; // Replace this with your namespace
+declare(strict_types=1);
+
+namespace leo\test;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 
-class TestPlugin extends PluginBase{
+class TestPlugin extends PluginBase {
 
     /** @var Config */
     private $config;
 
-    public function onEnable(){
+    public function onEnable(): void {
         $this->saveDefaultConfig();
-        $this->config = $this->getConfig();
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
         $this->getLogger()->info("TestPlugin has been enabled.");
     }
 
-    public function onDisable(){
+    public function onDisable(): void {
         $this->getLogger()->info("TestPlugin has been disabled.");
     }
 
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
-        if(strtolower($command->getName()) === "test"){
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if (strtolower($command->getName()) === "test") {
             $message = $this->config->get("message", "Hello, this plugin is working");
             $sender->sendMessage($message);
             return true;
